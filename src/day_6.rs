7e50718d2b8edf7dc3f47b1c90b42978c2fa539a
jeanberniolles <jeanberniolles@gmail.com>
2025-01-03 07:33:19 +0000

    cch23: day 6 all

--- src/day_6.rs
+++ src/day_6.rs
@@ -1,5 +1,6 @@
 use actix_web::{post, HttpResponse};
 use serde::{Deserialize, Serialize};
+use tracing::info;
 
 #[derive(Serialize, Debug, PartialEq, Deserialize)]
 struct ElfSearch {
@@ -9,10 +10,7 @@ struct ElfSearch {
         deserialize = "shelf with no elf on it"
     ))]
     shelf_wo_elf: usize,
-    #[serde(rename(
-        serialize = "elf on a shelf",
-        deserialize = "elf on a shelf"
-    ))]
+    #[serde(rename(serialize = "elf on a shelf", deserialize = "elf on a shelf"))]
     elf_with_shelf: usize,
 }
 
@@ -23,7 +21,7 @@ async fn day6_search(text: String) -> HttpResponse {
     for i in 0..text.len() {
         if text[i..].starts_with("elf on a shelf") {
             elf_with_shelf += 1;
-        } else if text[i..].starts_with("shelf") && i>8 && text[i-8..i].contains("elf on a ") {
+        } else if text[i..].starts_with("shelf") && i > 8 && !text[i - 9..i].contains("elf on a ") {
             shelf_wo_elf += 1;
         }
     }
@@ -45,35 +43,30 @@ mod tests {
 
     #[actix_web::test]
     async fn task_2_test_2() {
-        let app = test::init_service(
-            App::new()
-                .service(day6_search)
-        )
-        .await;
+        let app = test::init_service(App::new().service(day6_search)).await;
 
         // Create request object
         let req = test::TestRequest::with_uri("/6")
-        .   insert_header(ContentType::plaintext())
+            .insert_header(ContentType::plaintext())
             .method(Method::POST)
             .set_payload("In Belfast I heard an elf on a shelf on a shelf on a ")
             .to_request();
 
         // Call application
-        let res : ElfSearch = test::call_and_read_body_json(&app, req).await;
-        assert_eq!(res, ElfSearch {
-            elf: 4,
-            shelf_wo_elf: 0,
-            elf_with_shelf: 2
-        });
+        let res: ElfSearch = test::call_and_read_body_json(&app, req).await;
+        assert_eq!(
+            res,
+            ElfSearch {
+                elf: 4,
+                shelf_wo_elf: 0,
+                elf_with_shelf: 2
+            }
+        );
     }
 
     #[actix_web::test]
     async fn task_2_test_3() {
-        let app = test::init_service(
-            App::new()
-                .service(day6_search)
-        )
-        .await;
+        let app = test::init_service(App::new().service(day6_search)).await;
 
         // Create request object
         let req = test::TestRequest::with_uri("/6")
@@ -83,11 +76,14 @@ mod tests {
             .to_request();
 
         // Call application
-        let res : ElfSearch = test::call_and_read_body_json(&app, req).await;
-        assert_eq!(res, ElfSearch {
-            elf: 16,
-            shelf_wo_elf: 2,
-            elf_with_shelf: 8
-        });
+        let res: ElfSearch = test::call_and_read_body_json(&app, req).await;
+        assert_eq!(
+            res,
+            ElfSearch {
+                elf: 16,
+                shelf_wo_elf: 2,
+                elf_with_shelf: 8
+            }
+        );
     }
 }
