97ffde938b3e85e9541dd2d1890f50dcf893214f
Jean Berniolles <jean@berniolles.fr>
2025-01-15 09:46:52 +0000

    cch23: day 21 wip

---
+++ src/day_21.rs
@@ -0,0 +1,28 @@
+use actix_web::{get, web, HttpRequest, HttpResponse};
+use s2::cellid::CellID;
+use tracing::info;
+
+#[get("/21/coords/{binary}")]
+async fn day21_coords(
+    req: HttpRequest,
+    path: web::Path<String>,
+) -> HttpResponse {
+    info!("> {}", req.path());
+    match u64::from_str_radix(&path, 2) {
+    Ok(id) => {
+        
+        let cell = CellID  (id) ;
+    }
+    Err(_) => todo!(),
+    }
+    HttpResponse::BadRequest().finish()
+}
+
+#[get("/21/country/{binary}")]
+async fn day21_country(
+    req: HttpRequest,
+    path: web::Path<String>,
+) -> HttpResponse {
+    info!("> {}", req.path());
+    HttpResponse::BadRequest().finish()
+}
